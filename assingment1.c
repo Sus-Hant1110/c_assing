1dc645475594c90639eb15c2be0ee6e1b24a5261
Sus-Hant1110 <nsushaant72@gmail.com>
2024-08-08 07:13:08 +0545

    upadted

--- assingment1.c
+++ assingment1.c
@@ -2,7 +2,7 @@
 
 int main() {
     float r1, q1, r2, q2;
-    float t1, t2, average;
+    float total1, total2, average;
 
     printf("Enter  rate and quantity for first item: ");
     scanf("%f %f", &r1, &q1);
@@ -10,10 +10,10 @@ int main() {
     printf("Enter  rate and quantity for second item: ");
     scanf("%f %f", &r2, &q2);
 
-    t1 = r1 * q1;
-    t2 = r2 * q2;
+    total1 = r1 * q1;
+    total2 = r2 * q2;
 
-    average = (t1 + t2) / 2;
+    average = (total1 + total2) / 2;
 
     printf("The average value is: %.2f\n", average);
 
