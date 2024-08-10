3c4163bb620a4c6798447985b0a31da2dcb30861
Sus-Hant1110 <nsushaant72@gmail.com>
2024-08-10 18:01:45 +0545

    update

---
+++ assingment4.c
@@ -0,0 +1,14 @@
+// WAP to calculate the area of circle.(without taking input from the user)
+
+#include <stdio.h>
+
+int main()
+{
+    float radius = 5.0;
+
+    float area = 3.14 * radius * radius;
+
+    printf("Area of circle is: %f\n", area);
+
+    return 0;
+}
