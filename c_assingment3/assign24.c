a9a9528f19f99e4839328c46ec07d26cafc2a1bc
Sus-Hant1110 <sushantneupane@divyagyan.edu.np>
2024-09-04 10:35:39 +0545

    sn

--- c_assingment3/assign24.c
+++ c_assingment3/assign24.c
@@ -1,16 +1,17 @@
 // WAP to read a 3 digit no. and print each digit separately using loop
 
 #include <stdio.h>
-int main() {
+int main()
+{
     int n;
     printf("Enter a 3 digit number: ");
     scanf("%d", &n);
     int i = 0;
-    while (n > 0) {
+    while (n > 0)
+    {
         i = n % 10;
         printf("%d\n", i);
         n = n / 10;
     }
     return 0;
 }
-
