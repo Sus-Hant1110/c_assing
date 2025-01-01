c993e34ab5c1684c46e2e6678479724d7b52e98b
Sus-Hant1110 <sushantneupane@divyagyan.edu.np>
2025-01-01 23:04:40 +0545

    done

--- c_assingment4/assign36.c
+++ c_assingment4/assign36.c
@@ -5,32 +5,47 @@
 int main()
 {
 
-    int arr[10];
-    int i, j, temp;
+    int n, i, j, min_idx, temp;
 
-    printf("Enter 10 numbers: \n");
+    // Ask user for the number of elements in the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
 
-    for (i = 0; i < 10; i++)
+    int arr[n]; // Declare an array of size n
+
+    // Input array elements from the user
+    printf("Enter %d numbers: \n", n);
+    for (i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
-    for (i = 0; i < 10; i++)
+    // Selection Sort Algorithm
+    for (i = 0; i < n - 1; i++)
     {
-        for (j = i + 1; j < 10; j++)
+        min_idx = i; // Assume the first unsorted element is the minimum
+
+        // Find the minimum element in the unsorted part of the array
+        for (j = i + 1; j < n; j++)
         {
-            if (arr[i] > arr[j])
+            if (arr[j] < arr[min_idx])
             {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                min_idx = j;
             }
+        }
+
+        // Swap the found minimum element with the current element
+        if (min_idx != i)
+        {
+            temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
         }
     }
 
+    // Output the sorted array
     printf("Sorted array: \n");
-
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
--- c_assingment4/assign37.c
+++ c_assingment4/assign37.c
@@ -5,32 +5,39 @@
 int main()
 {
 
-    int arr[10];
-    int i, j, temp;
+    int n, i, j, temp;
 
-    printf("Enter 10 numbers: \n");
+    // Ask user for the number of elements in the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
 
-    for (i = 0; i < 10; i++)
+    int arr[n]; // Declare an array of size n
+
+    // Input array elements from the user
+    printf("Enter %d numbers: \n", n);
+    for (i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
-    for (i = 0; i < 10; i++)
+    // Bubble Sort Algorithm
+    for (i = 0; i < n - 1; i++)
     {
-        for (j = i + 1; j < 10; j++)
+        for (j = 0; j < n - i - 1; j++)
         {
-            if (arr[i] > arr[j])
+            if (arr[j] > arr[j + 1])
             {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                // Swap adjacent elements if they are in the wrong order
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 
+    // Output the sorted array
     printf("Sorted array: \n");
-
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
